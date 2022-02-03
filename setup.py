c80fbcc49a2be0f32e46b7c4824ff909a92bba83
benpankow <ben@elementl.com>
2022-02-03 10:58:08 -0800

    remove extra code

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="basic_ml",
-    packages=setuptools.find_packages(exclude=["basic_ml_tests"]),
+    packages=setuptools.find_packages(),
     install_requires=[
         "dagster==0.13.16",
         "dagit==0.13.16",
