c735ccea7b134c773c69dc364a808faecf3c0b9e
Ben Pankow <ben@elementl.com>
2022-02-03 08:25:24 -0800

    Update repository.py

--- basic_ml/repository.py
+++ basic_ml/repository.py
@@ -1,8 +1,6 @@
 from dagster import repository
 
-from basic_ml.jobs.say_hello import say_hello_job
-from basic_ml.schedules.my_hourly_schedule import my_hourly_schedule
-from basic_ml.sensors.my_sensor import my_sensor
+from .basic_ml import basic_ml_job
 
 
 @repository
@@ -13,8 +11,4 @@ def basic_ml():
     For hints on building your Dagster repository, see our documentation overview on Repositories:
     https://docs.dagster.io/overview/repositories-workspaces/repositories
     """
-    jobs = [say_hello_job]
-    schedules = [my_hourly_schedule]
-    sensors = [my_sensor]
-
-    return jobs + schedules + sensors
+    return [basic_ml_job]
